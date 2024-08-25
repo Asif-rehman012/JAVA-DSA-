47131acf90dde8f3aafbc5865cf3dd55c1d82b80
Asif Rehman <71745081+Asif-rehman012@users.noreply.github.com>
2024-08-25 22:09:23 +0530

    Add files via upload

---
+++ JAVA - DSA [COMPLETE]/2.Variables & DataStructure/javaBasics.java
@@ -0,0 +1,141 @@
+// import java.util.Scanner; // util package is imported after making scanner obj
+
+
+
+public class javaBasics{ // public class and jAva file should have same name
+
+
+
+  public static void main(String args[])
+{ // this is main function
+// System.out.println("hello Aasif "); //print is function
+// System.out.println("heloo bro"); //ln means linespace 
+// System.out.print("lovely professional university\n"); //backslahn means new line
+// System.out.println("9906558877");
+//boilerplate code
+  
+
+
+  //QUESTION :::::::: Print A pattern **** *** ** *
+
+  // System.out.println("****");
+  // System.out.println("***");
+  // System.out.println("**");
+  // System.out.println("*");
+
+  //lecture 5
+  // int a=5;
+  // int b= 4;
+  // int c=859;
+  // System.out.println(a);
+  // System.out.println(b);
+
+  // a=56; // value of a can be changed
+  // System.out.println(a);
+  // a=c;  // value of c assigned to a
+  // System.out.println(a);
+
+  //Lecture 6
+  // byte d=4;
+  // float e=45.4f;
+  // long l=4545555;
+  // double dd=452.5655d;
+  // System.out.println("PRINT ALL\n" +d+"\n"+e+"\n"+l+"\n"+dd);
+
+
+  // Lecture 7
+  // int a =45;
+  // int b=56;
+  // int sum= a+b; //creating sum variable
+  // System.out.println("Print the sum of A and B :--");
+  // System.out.println("a+b="+ sum );
+
+  // lecture 9
+  // Scanner sc = new Scanner(System.in);// sc object is created [system.in takes input]
+  // String input = sc.next(); // string input stores the input
+  // System.out.println(input);
+  
+  // String tagline = sc.nextLine(); //will take line as input and will print it
+  // System.out.println(tagline);
+  
+  // float roomSize = sc.nextFloat();
+  // System.out.println(roomSize);
+
+
+   // lecture 10 SUM OF A & B FROM USER INPUT
+  // System.out.println("Print the value of a =");
+  // int a = sc.nextInt();
+  // System.out.println(a);
+  
+  // System.out.println("print the value of b=");
+  // int b = sc.nextInt();
+  // System.out.println(b);
+  
+  // int sum = a+b;
+  // System.out.println("The sum of a+b =" + sum);
+
+  // lecture 11 Product of a & b
+  //  float a= sc.nextFloat();
+  //  float b= sc.nextFloat();
+  //  float product = a*b;
+  //  System.out.println(product);
+
+
+  //lecture 12 AREA OF CIRCLE ? (pie R square)
+  // float radius = sc.nextFloat(); //java takes decimal value as double so plz mention 4.22f
+  // float AREA = 3.14f* radius*radius;
+  // System.out.println(AREA);
+
+
+  //lecture 13  TYPE CONVERSION
+  // int a= 77;
+  // float c= 4.12f;
+  // byte b =a; // cannot convert from int to byte datatype
+  // long l= a; //type conversion from smaller dt to large one accepted
+  // System.out.println(l);
+  // System.out.println(c);
+
+  // int number = sc.nextFloat();// cannot convert float to int
+  // long aa= sc.nextInt(); //loosely conversion
+  // System.out.println(number);
+  // System.out.println(aa);
+  
+
+
+  //  Lecture 14 TYPE CASTING
+  //  float a= 12.55f;
+  //  int b= (int)a; // 0.55 data can be lost here in casting
+  //  System.out.println(b);
+
+    //  char car1= 'a';
+    //  char car2 = 'b';
+    //    int number1 = car1;
+    //    int number2 = car2;
+    //    System.out.println(number1); // a =97
+    //    System.out.println(number2); // b=98
+
+
+    // Lecture 15 Type promotion Expression
+  //   char a= 'a';
+  //   char b = 'b';
+  //   System.out.println((int)(b));
+  //   System.out.println((int)(a));
+  //   System.out.println(a);
+  //   System.out.println(b-a);//type expression only work with +-*
+
+  // short x=44;
+  // byte y=22;
+  // // byte bt =(x+y);// java automaticall converts them into int
+  // byte bt = ((byte)(x+y)); // type casting
+  // System.out.println(bt); //can print random answer
+
+    
+
+  // int a =10;
+  // float b =5.25f;
+  // double d= 254;
+  // // int ans = a + b + d; //all values converted to double
+  // System.out.println(ans);
+  }}
+
+
