0173fc5abfdeaff6bb0b09ce9bd9212f510f3a0b
Asif Rehman <71745081+Asif-rehman012@users.noreply.github.com>
2025-09-08 10:41:50 +0530

    Add files via upload

---
+++ JAVA - DSA [COMPLETE]/6.Patterns (part-I)/pattern.java
@@ -0,0 +1,48 @@
+
+public class pattern {
+    public static void main(String[] args) {
+
+        // ! L2 Print star pattern
+        // yah ek line print krai ga fir bda dai ga line ko
+        // for (int line = 1; line <= 4; line++) {
+        // for (int star = 1; star <= line; star++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // // har line print honai pai mujai next line chahiyai
+        // }
+
+        // ! L3 Print Inverted star
+
+        // int n = 4;
+        // for (int line = 1; line <= n; line++) {
+        // for (int star = 1; star <= n - line + 1; star++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // ! L4 print half-pyramid pattern
+        // int n = 4;
+
+        // for (int line = 1; line <= n; line++) {
+        // // numbers print
+        // for (int number = 1; number <= line; number++) {
+        // System.out.print (number);
+        // }
+        // System.out.println();
+        // }
+
+        // ! Print CHARACTER pattern
+        // int n = 4;
+        // char ch = 'A';
+
+        // for (int line = 1; line <= n; line++) {
+        // for (int charater = 1; charater <= line; charater++) {
+        // System.out.print(ch);
+        // ch++;
+        // }
+        // System.out.println();
+        // }
+    }
+}
