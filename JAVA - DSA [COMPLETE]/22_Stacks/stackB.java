c38cd1b0d07d703aa319846ea532bfa08e677888
Aasif <aasifrehman012@gmail.com>
2025-09-24 10:19:45 +0530

    Stacks solution uploaded

--- JAVA - DSA [COMPLETE]/22_Stacks/stackB.java
+++ JAVA - DSA [COMPLETE]/22_Stacks/stackB.java
@@ -139,25 +139,130 @@ public class stackB {
     // }
 
     // ! L8 Stock Span Problem
-    public static void stockSpan(int stocks[], int span[]) {
+    // public static void stockSpan(int stocks[], int span[]) {
+    // Stack<Integer> s = new Stack<>();
+    // span[0] = 1;
+    // s.push(0);
+
+    // for (int i = 1; i < stocks.length; i++) {
+    // int currPrice = stocks[i];
+    // while (!s.isEmpty() && currPrice > stocks[s.peek()]) {
+    // s.pop();
+
+    // }
+    // if (s.isEmpty()) {
+    // span[i] = i + 1;
+    // } else {
+    // int prevHigh = s.peek();
+    // span[i] = i - prevHigh;
+    // }
+    // s.push(i);
+    // }
+    // }
+
+    // ! L11 L12 VALID Paranthesis code
+    // public static boolean isvalid(String str) {
+    // Stack<Character> s = new Stack<>();
+
+    // for (int i = 0; i < str.length(); i++) {
+    // char ch = str.charAt(i);
+
+    // if (ch == '(' || ch == '{' || ch == '[') { // opening
+    // s.push(ch);
+    // } else {
+    // // closing
+    // if (s.isEmpty()) {
+    // return false;
+    // }
+    // if ((s.peek() == '(' && ch == ')') // ()
+    // || (s.peek() == '{' && ch == '}') // {}
+    // || (s.peek() == '{' && ch == ']')) {
+    // s.pop();
+
+    // } else {
+    // return false;
+    // }
+    // }
+
+    // }
+
+    // if (s.isEmpty()) {
+    // return true;
+    // } else {
+    // return false;
+    // }
+    // }
+
+    // ! L13 L14 Find duplicate parenthesis
+    // public static boolean isDuplicate(String str) {
+    // Stack<Character> s = new Stack<>();
+    // for (int i = 0; i < str.length(); i++) {
+    // char ch = str.charAt(i);
+
+    // // closing
+    // if (ch == ')') {
+    // int count = 0;
+    // while (s.pop() != '(') {
+
+    // count++;
+    // }
+    // if (count < 1) {
+    // return true; // duplicates
+    // }
+    // } else {
+    // // opening
+    // s.push(ch);
+    // }
+    // }
+
+    // return false;
+
+    // }
+
+    // ! L15 L16 MAXIMUM Rec area in histogram
+    public static void maxArea(int arr[]) {
+        int maxArea = 0;
+        int nsr[] = new int[arr.length];
+        int nsl[] = new int[arr.length];
+
+        // Next smaller right
         Stack<Integer> s = new Stack<>();
-        span[0] = 1;
-        s.push(0);
 
-        for (int i = 1; i < stocks.length; i++) {
-            int currPrice = stocks[i];
-            while (!s.isEmpty() && currPrice > stocks[s.peek()]) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
                 s.pop();
-
             }
             if (s.isEmpty()) {
-                span[i] = i + 1;
+                nsr[i] = arr.length;
             } else {
-                int prevHigh = s.peek();
-                span[i] = i - prevHigh;
+                nsr[i] = s.peek();
             }
             s.push(i);
         }
+
+        // Next smaller left
+        s = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nsl[i] = -1;
+            } else {
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
+        // Current Area : width = j-i-1 = nsr[i] - nsl[i]-1
+        for (int i = 0; i < arr.length; i++) {
+            int height = arr[i];
+            int width = nsr[i] - nsl[i] - 1;
+            int currArea = height * width;
+            maxArea = Math.max(currArea, maxArea);
+        }
+
+        System.out.println("max area in histogram = " + maxArea);
     }
 
     public static void main(String[] args) {
@@ -191,12 +296,52 @@ public class stackB {
         // // 1, 2, 3
 
         // L8
-        int stocks[] = { 100, 80, 60, 70, 60, 85, 100 };
-        int span[] = new int[stocks.length];
-        stockSpan(stocks, span);
+        // int stocks[] = { 100, 80, 60, 70, 60, 85, 100 };
+        // int span[] = new int[stocks.length];
+        // stockSpan(stocks, span);
 
-        for (int i = 0; i < span.length; i++) {
-            System.out.println(span[i] + " ");
-        }
+        // for (int i = 0; i < span.length; i++) {
+        // System.out.println(span[i] + " ");
+        // }
+        // ! L9 L10 Next Greater Element
+        // int arr[] = { 6, 8, 0, 1, 3 };
+        // Stack<Integer> s = new Stack<>();
+        // int nxtGreater[] = new int[arr.length];
+
+        // for (int i = arr.length - 1; i >= 0; i--) {
+        // // 1. Run while loop
+        // while (!s.isEmpty() && arr[s.peek()] <= arr[i]) {
+        // s.pop();
+        // }
+        // // 2 if-else
+        // if (s.isEmpty()) {
+        // nxtGreater[i] = -1;
+        // } else {
+        // nxtGreater[i] = arr[s.peek()];
+        // }
+
+        // // 3 push in s
+        // s.push(i);
+        // }
+
+        // for (int i = 0; i < nxtGreater.length; i++) {
+        // System.out.println(nxtGreater[i] + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // L10 Next Greater
+        // String str = "(({})[]";
+        // System.out.println(isvalid(str));
+
+        // L12 Find duplicate parentheses
+        // String str = "((a+b))"; // true (duplicates found)
+        // String str2 = "(a-b)"; // false
+        // System.out.println(isDuplicate(str));
+
+        // L15
+        int arr[] = { 2, 1, 5, 6, 2, 3 }; // heights in histogram
+        maxArea(arr);
+
     }
 }
