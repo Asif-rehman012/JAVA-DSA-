0482b3de438eb0fef08816b5023c202265c8b5c5
Aasif <aasifrehman012@gmail.com>
2025-10-14 23:04:44 +0530

    folder hasing uploaded

---
+++ JAVA - DSA [COMPLETE]/31.Hashing/hasingB.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class hasingB {
+
+    public static void main(String args[]) {
+        // ! Lec - 03 (Hashmap Operations)
+        // // create
+        // HashMap<String, Integer> hm = new HashMap<>();
+
+        // // Insert
+        // hm.put("India", 100);
+        // hm.put("China", 150);
+        // hm.put("US", 50);
+
+        // System.out.println(hm);
+
+        // // Get - O(1)
+        // int population = hm.get("India");
+        // System.out.println(population);
+
+        // System.out.println(hm.get("Indonesia"));
+
+        // // ContainsKey - O(1)
+        // System.out.println(hm.containsKey("India")); // true
+        // System.out.println(hm.containsKey("Indonesia")); // false
+
+        // // Remove
+        // System.out.println(hm.remove("China"));
+        // System.out.println(hm);
+
+        // // size
+        // System.out.println(hm.size());
+
+        // // Is Empty
+        // hm.clear(); // sara clear krta hai
+        // System.out.println(hm.isEmpty()); // mara hashmap khali nhi hai isiliyai
+        // false ayai ga
+
+        // ! Lecture 4 (Iteration on Hashmap)
+        // HashMap<String, Integer> hm = new HashMap<>();
+        // hm.put("India", 100);
+        // hm.put("China", 150);
+        // hm.put("US", 50);
+        // hm.put("Indonesia", 6);
+        // hm.put("Nepal", 5);
+
+        // // Iterate
+        // Set<String> keys = hm.keySet();
+        // System.out.println(keys);
+
+        // for (String k : keys) {
+        // System.out.println("key=" + k + ",value=" + hm.get(k));
+        // }
+
+    }
+}
