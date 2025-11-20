65e6d5cd9bf193b64be887aaaa57c2eca4f948ae
Aasif Rehman <71745081+Asif-rehman012@users.noreply.github.com>
2025-11-20 20:31:01 +0530

    Update dp_part2.java

--- JAVA - DSA [COMPLETE]/38.DP(part-2)/dp_part2.java
+++ JAVA - DSA [COMPLETE]/38.DP(part-2)/dp_part2.java
@@ -44,6 +44,111 @@ public class dp_part2 {
 }
 
 
+//! lec-04 ... 0-1 Knapsack(Tabulation code)
+//it will print the table
+// public static void print(int dp[][]){
+//     for(int i=0; i<dp.length; i++){
+//        for(int j=0; j<dp[0].length; j++){
+//            System.out.print(dp[i][j]+ " ");
+//        }
+//        System.out.println();
+//     }
+//     System.out.println();
+// }
+//    public static int knapsackTab(int val[], int wt[], int W){
+//       int n = val.length;
+//       int dp[][] = new int [n+1][W+1];
+//       for(int i=0; i<dp.length; i++){
+//         dp[i][0] = 0;
+//       }
+//       for(int j=0; j<dp[0].length; j++){
+//          dp[0][j] =0;
+//       }
+
+//       for(int i=1; i<n+1; i++){
+//           for(int j=1; j<W+1; j++){
+//              int v = val[i-1]; // ith item val
+//              int w = wt[i-1]; // ith item wt
+//              if (w <= j) { // valid
+//                  int incProfit = v + dp[i-1][j-w];
+//                  int excProfit = dp[i-1][j];
+//                  dp[i][j] = Math.max(incProfit, excProfit);
+
+              
+//              }else{
+//                 int excProfit = dp[i-1][j];
+//                 dp[i][j] = excProfit;
+//              }
+//           }
+//       }
+//         print(dp);
+//        return dp[n][W];
+//    }
+
+  //! lec-06 Target Sum SubSet (code )
+  // public static boolean targetSumSubset(int arr[], int sum){
+  //   int n = arr.length;
+  //   boolean dp[][] = new boolean[n+1][sum+1];
+  //   // i=items & j=target sum
+  //   for(int i=0; i<n+1; i++){
+  //     dp[i][0] = true;
+  //   }
+
+  //   for(int i=1; i<n+1; i++){
+  //      for(int j=1; j<sum+1; j++){
+  //        int v = arr[i-1];
+  //        //include
+  //        if (v <= j && dp[i-1][j-v] == true) {
+  //           dp[i][j] = true;
+  //        }
+  //        //exclude
+  //        else if (dp[i-1][j] == true) {
+  //            dp[i][j] = true;
+  //        }
+  //      }
+  //   }
+  //    print(dp);
+  //   return dp[n][sum];
+  // }
+
+  // public static void print(boolean dp[][]){
+  //    for(int i=0; i<dp.length; i++){
+  //     for(int j=0; j<dp[0].length; j++){
+  //        System.out.print(dp[i][j]+ " ");
+  //     }
+  //     System.out.println();
+  //    }
+  //    System.out.println();
+  // }
+
+  //! lec-07 Unbounded knapsack (Tabulation code)
+
+  public static int unboundedKnapsack(int val[], int wt[], int W){
+    int n = val.length;
+    int dp[][] = new int [n+1][W+1];
+
+    for (int i=0; i<n+1; i++){
+        dp[i][0] = 0;
+    }
+    for(int j=0; j<W+1; j++){
+        dp[0][j] = 0;
+    }
+
+      for(int i=1; i<n+1; i++){
+          for(int j=1; j<W+1; j++){
+             if (wt[i-1] <= j) { //valid
+                dp[i][j] = Math.max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
+              
+             }else{ //invalid
+                 dp[i][j] = dp[i-1][j];
+             }
+          }
+      }
+
+       return dp[n][W];
+  }
+
+
 public static void main(String[] args) {
 //     int val[] = {15, 14, 10, 45, 30};
 //     int wt[] = {2, 5, 1, 3, 4};
@@ -54,19 +159,34 @@ public static void main(String[] args) {
 // }
 
 //! lec-03 main code
-    int val[] = {15, 14, 10, 45, 30};
-    int wt[] = {2, 5, 1, 3, 4};
-    int W=7;
+    // int val[] = {15, 14, 10, 45, 30};
+    // int wt[] = {2, 5, 1, 3, 4};
+    // int W=7;
 
-    int dp[][] = new int[val.length+1][W+1];
-    for(int i=0; i<dp.length; i++){
-        for(int j=0; j<dp[0].length; j++){
-            dp[i][j] = -1;
-        }
-    }
+    // int dp[][] = new int[val.length+1][W+1];
+    // for(int i=0; i<dp.length; i++){
+    //     for(int j=0; j<dp[0].length; j++){
+    //         dp[i][j] = -1;
+    //     }
+    // }
 
-    System.out.println(knapsack(val, wt, W, val.length, dp));
-}
+    // System.out.println(knapsack(val, wt, W, val.length, dp));
+
+    //  System.out.println("The final value = " + knapsackTab(val, wt, W));
+ 
+ 
+     //! lec 06 main drive code
+    //  int arr[] = {4, 2, 7, 1, 3};
+    //  int sum = 10;
+    //  System.out.println(targetSumSubset(arr, sum));
+
+    //!lec 07 main driver code
+     int val[] = {15, 14, 10, 45, 30};
+     int wt[] = {2, 5, 1, 3, 4};
+     int W=7;
+
+     System.out.println("unbounded final answer =" + unboundedKnapsack(val, wt, W));
+  }
 
 
 
